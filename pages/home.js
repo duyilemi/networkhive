c92ccd33872373f16cedcf347e3734797cc2aba9
charles olajide <charlesduyilemi@gmail.com>
2023-04-22 09:03:38 -0700

    modified:   pages/home.js

--- pages/home.js
+++ pages/home.js
@@ -17,7 +17,7 @@ function Home({ providers }) {
       </Head>
       <header className="flex justify-around items-center py-4">
         <div className="relative w-36 h-10">
-          <Image src="https://rb.gy/0m09u" layout="fill" objectFit="contain" />
+          <Image src="/Linkedin-logo.png" layout="fill" objectFit="contain" />
         </div>
         <div className="flex items-center sm:divide-x divide-gray-300">
           <div className="hidden sm:flex space-x-8 pr-4">
